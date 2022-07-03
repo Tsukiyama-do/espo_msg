d4a3d44509cd1a5f220ab599c9b81dbfd8dd4492
Tsukiyama-do <yuichi.sr.hirose@gmail.com>
2022-07-03 17:01:06 +0900

    7

--- awsSes.js
+++ awsSes.js
@@ -8,7 +8,6 @@ const awsSes =  ( msg ) => {
       // a client can be shared by different commands.
       const client = new SESClient({ region: "us-east-1" ,credentials: fromIni({profile: "yuichi01"})});
 
-
       const params = {
         Destination: {
           /* required */
@@ -16,7 +15,8 @@ const awsSes =  ( msg ) => {
             /* more items */
           ],
           ToAddresses: [
-            "yuichisierra_kusc_fm@willcom.com", //RECEIVER_ADDRESS
+//            "yuichisierra_kusc_fm@willcom.com", //RECEIVER_ADDRESS
+            "yuichi.sr.hirose@gmail.com", //RECEIVER_ADDRESS
             /* more To-email addresses */
           ],
         },
@@ -35,7 +35,7 @@ const awsSes =  ( msg ) => {
           },
           Subject: {
             Charset: "UTF-8",
-            Data: "EMAIL_ESPO",
+            Data: msg,
           },
         },
         Source: "noreply@jj1pow.com", // SENDER_ADDRESS
@@ -51,10 +51,10 @@ const awsSes =  ( msg ) => {
         try {
           const data = await client.send(new SendEmailCommand(params));
 //          console.log("Success", data);
-          return "Success" + data ;
+          return "Message is sent in success"  ;
         } catch (err) {
 //          console.log("Error", err);
-          return "Error" + err;
+          return "Error occurred in sending the message." ;
 }
       };
       run();
