d0fb841d5179fd2e1b8259740da7d4ab4ff81235
Tsukiyama-do <yuichi.sr.hirose@gmail.com>
2022-07-03 17:37:46 +0900

    fix respo

--- index.js
+++ index.js
@@ -14,15 +14,20 @@ let deci = '';
 let dvalue = '';
 // Http.onreadystatechange = () => console.log(Http.responseText)
 Http.onreadystatechange = () => { 
-  const strwk = Http.responseText 
-  const kok_position = strwk.indexOf('Kok') 
-  const yam_position = strwk.indexOf('Yam') 
-//        console.log(strwk.substring(yam_position - 2,yam_position - 1 )) 
-    deci = strwk.substring(yam_position - 2,yam_position - 1 )
-    dvalue = "Espo : " + strwk.substring(yam_position - 6,yam_position - 1 )
-    deci === '*' ? console.log(awsSes(dvalue )) : console.log("NoEspo")
 
-  }
+  if (Http.readyState == 4 // 通信の完了
+    && Http.status == 200) { // 通信の成功
+
+      const strwk = Http.responseText 
+      const kok_position = strwk.indexOf('Kok') 
+      const yam_position = strwk.indexOf('Yam') 
+    //        console.log(strwk.substring(yam_position - 2,yam_position - 1 )) 
+        deci = strwk.substring(yam_position - 2,yam_position - 1 )
+        dvalue = "Espo : " + strwk.substring(yam_position - 6,yam_position - 1 )
+        deci === '*' ? console.log(awsSes(dvalue )) : console.log("NoEspo")
+    }
+
+}
 
 // Http.onreadystatechange = (e) => {
 //    console.log(Http.responseText)
